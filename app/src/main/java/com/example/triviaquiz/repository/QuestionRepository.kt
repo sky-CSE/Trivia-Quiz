aa0a130b3d0d27937d0b52a077051bcf3fd31eb0
Sumeet Yadav <ysumeet35@gmail.com>
2022-05-20 01:51:37 +0530

    Repository Finished

--- app/src/main/java/com/example/triviaquiz/repository/QuestionRepository.kt
+++ app/src/main/java/com/example/triviaquiz/repository/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package com.example.triviaquiz.repository
 
+import android.util.Log
 import com.example.triviaquiz.data.DataOrException
 import com.example.triviaquiz.model.QuestionItem
 import com.example.triviaquiz.network.QuestionApi
@@ -7,6 +8,7 @@ import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val api : QuestionApi){
     //API is like DAO
+
     /*listOfQuestions = ArrayList<QuestionItem>() could be able to hold questions too
     * but ArrayList<QuestionItem> can't add any metadata about the response we are getting from server
     * So,we used a WRAPPER class to wrap this arraylist into a different object
@@ -14,8 +16,23 @@ class QuestionRepository @Inject constructor(private val api : QuestionApi){
     * if Boolean if true, it means the data is loading
     * if any Exceptions or error occurred, Exception will tell us
     */
-    private val listOfQuestions =
+
+    //listOfQuestions renamed to 'dataOrException' as it is inside it
+    private val dataOrException =
         DataOrException<ArrayList<QuestionItem>,Boolean,Exception>()
+
+    suspend fun getAllQuestions() : DataOrException<ArrayList<QuestionItem>,Boolean,Exception>{
+        try{
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+        }catch (exception: Exception){
+            dataOrException.e = exception
+            Log.d("Exc","getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+        }
+
+        return dataOrException
+    }
 
 
 }
